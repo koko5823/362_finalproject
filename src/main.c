49e46380aed6602ecd90e535a77de89841a39313
0.0 <33805073+koko5823@users.noreply.github.com>
2024-11-15 15:43:44 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -39,14 +39,36 @@ void Timer2_init(void)
 }
 
 
+void Timer2_init(void)
+{
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+
+
+    GPIOA->MODER &= ~(0xFF);
+    GPIOA->MODER |= 0xAA;
+    GPIOA->AFR[0] &= ~(0xFFFF);
+    GPIOA->AFR[0] |= 0x2222;
+
+
+    TIM2->PSC = 479;
+    TIM2->ARR = 1999;
+    TIM2->CCMR1 |= TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1;
+    TIM2->CCMR1 &= ~TIM_CCMR1_OC1M_0;
+    TIM2->CCER |= TIM_CCER_CC1E;
+    TIM2->CR1 |= TIM_CR1_CEN;
+}
+
+
 void rotate(void)
 {
-    TIM2->CCR1 = 15; // set the servo to 90 degrees
-    //TIM2->CCR1 = 5;
-    //15 is 0 degree
-    //0 is 90 degree
-    //nano_wait(1000000000); // wait for 1 second
+    TIM2->CCR1 = 150;// rotate0
+    nano_wait(2000000000); // wait for 1 second
     //TIM2->CCR1 = 200; // set the servo to 180 degrees
+}
+void rotate1(void)//rotate to 90 degrees
+{
+    TIM2->CCR1 = 250; // set the servo to 180 degrees
 }
 //===========================================================================
 
@@ -325,14 +347,7 @@ int main(void) {
     internal_clock();
     Timer2_init();
     rotate();
-    msg[0] |= font['E'];
-    msg[1] |= font['C'];
-    msg[2] |= font['E'];
-    msg[3] |= font[' '];
-    msg[4] |= font['3'];
-    msg[5] |= font['6'];
-    msg[6] |= font['2'];
-    msg[7] |= font[' '];
+    rotate1();
 
     // GPIO enable
     enable_ports();
